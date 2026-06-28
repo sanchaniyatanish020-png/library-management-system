d0aed19acf8653a7ed8a1f04acaa2d3db43b1006
Tanish Sanchaniya <sanchaniyatanish020@gmail.com>
2026-06-28 18:51:10 +0530

    fix: CORS preflight and OPTIONS handler

--- backend/src/Middleware/CorsMiddleware.php
+++ backend/src/Middleware/CorsMiddleware.php
@@ -5,13 +5,25 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response as SlimResponse;
 
 class CorsMiddleware implements MiddlewareInterface {
     public function process(Request $request, RequestHandler $handler): Response {
+        
+        // Handle OPTIONS preflight request
+        if ($request->getMethod() === 'OPTIONS') {
+            $response = new SlimResponse();
+            return $response
+                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->withStatus(200);
+        }
+
         $response = $handler->handle($request);
         return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
