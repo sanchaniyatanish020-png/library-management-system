c598808751185b9bd643590f8d77e103ef2d39a6
Tanish Sanchaniya <sanchaniyatanish020@gmail.com>
2026-06-28 18:39:45 +0530

    fix: CORS before routing

--- backend/src/Config/App.php
+++ backend/src/Config/App.php
@@ -3,7 +3,6 @@ namespace App\Config;
 
 use Slim\Factory\AppFactory;
 use Dotenv\Dotenv;
-use Slim\Routing\RouteCollectorProxy;
 
 class App {
     public static function create(): \Slim\App {
@@ -11,24 +10,26 @@ class App {
         $dotenv->safeLoad();
 
         $app = AppFactory::create();
+
+        $app->add(function ($request, $handler) {
+            if ($request->getMethod() === 'OPTIONS') {
+                $response = new \Slim\Psr7\Response();
+                return $response
+                    ->withHeader('Access-Control-Allow-Origin', '*')
+                    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+                    ->withStatus(200);
+            }
+            $response = $handler->handle($request);
+            return $response
+                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        });
+
         $app->addBodyParsingMiddleware();
         $app->addRoutingMiddleware();
         $app->addErrorMiddleware(true, true, true);
-
-        // CORS Middleware
-        $app->add(function ($request, $handler) {
-            $response = $handler->handle($request);
-            return $response
-                ->withHeader('Access-Control-Allow-Origin', 'https://library-management-system-three-rose-96.vercel.app')
-                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
-                ->withHeader('Access-Control-Allow-Credentials', 'true');
-        });
-
-        // Handle OPTIONS preflight
-        $app->options('/{routes:.+}', function ($request, $response) {
-            return $response;
-        });
 
         return $app;
     }
